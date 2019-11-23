bedc2fe2123c3dd668c024bdf6f4b2abdcc0f0bf
Alexandrith Sharron <alexsharon@gmail.com>
2019-11-23 02:34:05 +0000

    Basic Install Script

---
+++ start.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install vim
+sudo apt install git
+sudo apt install zsh
+sudo apt install curl
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
